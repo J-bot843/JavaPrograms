07ba6f6d8c5eb8ab81c361705a20ef1b1258b4c3
John <John.S123@gmail.com>
2020-05-13 15:58:09 -0700

    Added Random Number java file

---
+++ randNums.java
@@ -0,0 +1,12 @@
+import java.util.random
+
+public class generateRanNum {
+	public static void main(String args[])
+	{
+		Random random = new Random();
+		
+		int randomNum = random.nextInt(100);
+		
+		System.out.println("A Random Number between 0-99 is : " + randomNum);
+	}
+}
